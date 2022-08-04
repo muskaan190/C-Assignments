b55696ac3020f57b7c1cea433033d628ee0f2ebf
Muskan Makan <90406508+muskaan190@users.noreply.github.com>
2022-08-04 17:21:30 +0530

    Code Added

---
+++ Looping Assignment/ques_7.C
@@ -0,0 +1,13 @@
+// WAP for multiplication table generation.
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+
+    for(int i=1;i<=10;i++){
+        printf("%d x %d = %d\n",n,i,n*i);
+    }
+}
