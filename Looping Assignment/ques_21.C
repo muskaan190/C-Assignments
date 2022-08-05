ceeef298f263cb00d8f0bbbf221ee6503e93a737
Muskan Makan <90406508+muskaan190@users.noreply.github.com>
2022-08-05 22:13:04 +0530

    Add files via upload

---
+++ Looping Assignment/ques_21.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int fact(int n){
+    if(n==1 || n==0)
+        return 1;
+    else
+        return n*fact(n-1);
+}
+
+int main(){
+    float sum=0.0;
+    float f;
+    int x,n;
+
+    printf("Enter the value of x and n\n");
+    scanf("%d %d",&x,&n);
+    int temp=1;
+    int m=0;
+    for(int i=1;i<n;i++){
+        temp=temp*x;
+        f=(float)temp/fact(m+1);
+        sum+=f;
+        m++;
+    }
+
+    printf("%f",sum+1);
+}
